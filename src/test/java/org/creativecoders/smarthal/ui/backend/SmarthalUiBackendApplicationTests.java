a233daa944c5f7314be79b5376e1c23eb327e937
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-10-26 19:28:42 +0100

    Switch to AssertJ for assertions in `SmarthalUiBackendApplicationTests`

--- src/test/java/org/creativecoders/smarthal/ui/backend/SmarthalUiBackendApplicationTests.java
+++ src/test/java/org/creativecoders/smarthal/ui/backend/SmarthalUiBackendApplicationTests.java
@@ -1,20 +1,20 @@
 package org.creativecoders.smarthal.ui.backend;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.assertj.core.api.WithAssertions;
 
 @SpringBootTest
-class SmarthalUiBackendApplicationTests {
+class SmarthalUiBackendApplicationTests implements WithAssertions {
 
     @Autowired
     ApplicationContext context;
 
     @Test
     void contextLoads() {
-        assertNotNull(context);
+        assertThat(context).isNotNull();
     }
 
 }
